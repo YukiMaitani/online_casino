30a9df3cdb943bc72d8e66505bcecf7498748bb6
YukiMaitani <fightzoi1997@gmail.com>
2024-01-09 08:09:29 +0900

    トランプの画像を追加

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,364 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  $AssetsImagesCardGen get card => const $AssetsImagesCardGen();
+}
+
+class $AssetsImagesCardGen {
+  const $AssetsImagesCardGen();
+
+  /// File path: assets/images/card/club_1.png
+  AssetGenImage get club1 =>
+      const AssetGenImage('assets/images/card/club_1.png');
+
+  /// File path: assets/images/card/club_10.png
+  AssetGenImage get club10 =>
+      const AssetGenImage('assets/images/card/club_10.png');
+
+  /// File path: assets/images/card/club_11.png
+  AssetGenImage get club11 =>
+      const AssetGenImage('assets/images/card/club_11.png');
+
+  /// File path: assets/images/card/club_12.png
+  AssetGenImage get club12 =>
+      const AssetGenImage('assets/images/card/club_12.png');
+
+  /// File path: assets/images/card/club_13.png
+  AssetGenImage get club13 =>
+      const AssetGenImage('assets/images/card/club_13.png');
+
+  /// File path: assets/images/card/club_2.png
+  AssetGenImage get club2 =>
+      const AssetGenImage('assets/images/card/club_2.png');
+
+  /// File path: assets/images/card/club_3.png
+  AssetGenImage get club3 =>
+      const AssetGenImage('assets/images/card/club_3.png');
+
+  /// File path: assets/images/card/club_4.png
+  AssetGenImage get club4 =>
+      const AssetGenImage('assets/images/card/club_4.png');
+
+  /// File path: assets/images/card/club_5.png
+  AssetGenImage get club5 =>
+      const AssetGenImage('assets/images/card/club_5.png');
+
+  /// File path: assets/images/card/club_6.png
+  AssetGenImage get club6 =>
+      const AssetGenImage('assets/images/card/club_6.png');
+
+  /// File path: assets/images/card/club_7.png
+  AssetGenImage get club7 =>
+      const AssetGenImage('assets/images/card/club_7.png');
+
+  /// File path: assets/images/card/club_8.png
+  AssetGenImage get club8 =>
+      const AssetGenImage('assets/images/card/club_8.png');
+
+  /// File path: assets/images/card/club_9.png
+  AssetGenImage get club9 =>
+      const AssetGenImage('assets/images/card/club_9.png');
+
+  /// File path: assets/images/card/diamond_1.png
+  AssetGenImage get diamond1 =>
+      const AssetGenImage('assets/images/card/diamond_1.png');
+
+  /// File path: assets/images/card/diamond_10.png
+  AssetGenImage get diamond10 =>
+      const AssetGenImage('assets/images/card/diamond_10.png');
+
+  /// File path: assets/images/card/diamond_11.png
+  AssetGenImage get diamond11 =>
+      const AssetGenImage('assets/images/card/diamond_11.png');
+
+  /// File path: assets/images/card/diamond_12.png
+  AssetGenImage get diamond12 =>
+      const AssetGenImage('assets/images/card/diamond_12.png');
+
+  /// File path: assets/images/card/diamond_13.png
+  AssetGenImage get diamond13 =>
+      const AssetGenImage('assets/images/card/diamond_13.png');
+
+  /// File path: assets/images/card/diamond_2.png
+  AssetGenImage get diamond2 =>
+      const AssetGenImage('assets/images/card/diamond_2.png');
+
+  /// File path: assets/images/card/diamond_3.png
+  AssetGenImage get diamond3 =>
+      const AssetGenImage('assets/images/card/diamond_3.png');
+
+  /// File path: assets/images/card/diamond_4.png
+  AssetGenImage get diamond4 =>
+      const AssetGenImage('assets/images/card/diamond_4.png');
+
+  /// File path: assets/images/card/diamond_5.png
+  AssetGenImage get diamond5 =>
+      const AssetGenImage('assets/images/card/diamond_5.png');
+
+  /// File path: assets/images/card/diamond_6.png
+  AssetGenImage get diamond6 =>
+      const AssetGenImage('assets/images/card/diamond_6.png');
+
+  /// File path: assets/images/card/diamond_7.png
+  AssetGenImage get diamond7 =>
+      const AssetGenImage('assets/images/card/diamond_7.png');
+
+  /// File path: assets/images/card/diamond_8.png
+  AssetGenImage get diamond8 =>
+      const AssetGenImage('assets/images/card/diamond_8.png');
+
+  /// File path: assets/images/card/diamond_9.png
+  AssetGenImage get diamond9 =>
+      const AssetGenImage('assets/images/card/diamond_9.png');
+
+  /// File path: assets/images/card/heart_1.png
+  AssetGenImage get heart1 =>
+      const AssetGenImage('assets/images/card/heart_1.png');
+
+  /// File path: assets/images/card/heart_10.png
+  AssetGenImage get heart10 =>
+      const AssetGenImage('assets/images/card/heart_10.png');
+
+  /// File path: assets/images/card/heart_11.png
+  AssetGenImage get heart11 =>
+      const AssetGenImage('assets/images/card/heart_11.png');
+
+  /// File path: assets/images/card/heart_12.png
+  AssetGenImage get heart12 =>
+      const AssetGenImage('assets/images/card/heart_12.png');
+
+  /// File path: assets/images/card/heart_13.png
+  AssetGenImage get heart13 =>
+      const AssetGenImage('assets/images/card/heart_13.png');
+
+  /// File path: assets/images/card/heart_2.png
+  AssetGenImage get heart2 =>
+      const AssetGenImage('assets/images/card/heart_2.png');
+
+  /// File path: assets/images/card/heart_3.png
+  AssetGenImage get heart3 =>
+      const AssetGenImage('assets/images/card/heart_3.png');
+
+  /// File path: assets/images/card/heart_4.png
+  AssetGenImage get heart4 =>
+      const AssetGenImage('assets/images/card/heart_4.png');
+
+  /// File path: assets/images/card/heart_5.png
+  AssetGenImage get heart5 =>
+      const AssetGenImage('assets/images/card/heart_5.png');
+
+  /// File path: assets/images/card/heart_6.png
+  AssetGenImage get heart6 =>
+      const AssetGenImage('assets/images/card/heart_6.png');
+
+  /// File path: assets/images/card/heart_7.png
+  AssetGenImage get heart7 =>
+      const AssetGenImage('assets/images/card/heart_7.png');
+
+  /// File path: assets/images/card/heart_8.png
+  AssetGenImage get heart8 =>
+      const AssetGenImage('assets/images/card/heart_8.png');
+
+  /// File path: assets/images/card/heart_9.png
+  AssetGenImage get heart9 =>
+      const AssetGenImage('assets/images/card/heart_9.png');
+
+  /// File path: assets/images/card/spade_1.png
+  AssetGenImage get spade1 =>
+      const AssetGenImage('assets/images/card/spade_1.png');
+
+  /// File path: assets/images/card/spade_10.png
+  AssetGenImage get spade10 =>
+      const AssetGenImage('assets/images/card/spade_10.png');
+
+  /// File path: assets/images/card/spade_11.png
+  AssetGenImage get spade11 =>
+      const AssetGenImage('assets/images/card/spade_11.png');
+
+  /// File path: assets/images/card/spade_12.png
+  AssetGenImage get spade12 =>
+      const AssetGenImage('assets/images/card/spade_12.png');
+
+  /// File path: assets/images/card/spade_13.png
+  AssetGenImage get spade13 =>
+      const AssetGenImage('assets/images/card/spade_13.png');
+
+  /// File path: assets/images/card/spade_2.png
+  AssetGenImage get spade2 =>
+      const AssetGenImage('assets/images/card/spade_2.png');
+
+  /// File path: assets/images/card/spade_3.png
+  AssetGenImage get spade3 =>
+      const AssetGenImage('assets/images/card/spade_3.png');
+
+  /// File path: assets/images/card/spade_4.png
+  AssetGenImage get spade4 =>
+      const AssetGenImage('assets/images/card/spade_4.png');
+
+  /// File path: assets/images/card/spade_5.png
+  AssetGenImage get spade5 =>
+      const AssetGenImage('assets/images/card/spade_5.png');
+
+  /// File path: assets/images/card/spade_6.png
+  AssetGenImage get spade6 =>
+      const AssetGenImage('assets/images/card/spade_6.png');
+
+  /// File path: assets/images/card/spade_7.png
+  AssetGenImage get spade7 =>
+      const AssetGenImage('assets/images/card/spade_7.png');
+
+  /// File path: assets/images/card/spade_8.png
+  AssetGenImage get spade8 =>
+      const AssetGenImage('assets/images/card/spade_8.png');
+
+  /// File path: assets/images/card/spade_9.png
+  AssetGenImage get spade9 =>
+      const AssetGenImage('assets/images/card/spade_9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        club1,
+        club10,
+        club11,
+        club12,
+        club13,
+        club2,
+        club3,
+        club4,
+        club5,
+        club6,
+        club7,
+        club8,
+        club9,
+        diamond1,
+        diamond10,
+        diamond11,
+        diamond12,
+        diamond13,
+        diamond2,
+        diamond3,
+        diamond4,
+        diamond5,
+        diamond6,
+        diamond7,
+        diamond8,
+        diamond9,
+        heart1,
+        heart10,
+        heart11,
+        heart12,
+        heart13,
+        heart2,
+        heart3,
+        heart4,
+        heart5,
+        heart6,
+        heart7,
+        heart8,
+        heart9,
+        spade1,
+        spade10,
+        spade11,
+        spade12,
+        spade13,
+        spade2,
+        spade3,
+        spade4,
+        spade5,
+        spade6,
+        spade7,
+        spade8,
+        spade9
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
